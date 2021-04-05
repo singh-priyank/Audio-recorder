2a2c873a47a8078338e8cb676c62b604f268da97
singh-priyank <priyankkumar179@gmail.com>
2021-04-05 22:37:46 +0530

    initial

---
+++ upload.php
@@ -0,0 +1,14 @@
+<?php
+print_r($_FILES); //this will print out the received name, temp name, type, size, etc.
+
+
+$size = $_FILES['audio_data']['size']; //the size in bytes
+$input = $_FILES['audio_data']['tmp_name']; //temporary name that PHP gave to the uploaded file
+//$output = $_FILES['audio_data']['name'].".wav"; //letting the client control the filename is a rather bad idea
+
+$dir = 'uploads/';
+$audio_path = $dir.basename($_FILES['audio_data']['name']);
+
+//move the file from temp name to local folder using $output name
+move_uploaded_file($input, $audio_path)
+?>
